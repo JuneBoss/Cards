e9207f4cbd2ba210d14b7ca7918ccd01fc933578
JuneBoss <89157412+JuneBoss@users.noreply.github.com>
2022-03-21 17:01:16 -0600

    Finishing the different battle outcomes

--- src/cards/controller/Controller.java
+++ src/cards/controller/Controller.java
@@ -73,6 +73,160 @@ public class Controller
 				outcome = 2;
 			}
 			break;
+		case "4":
+			if(right == "2" || right == "3" || right == "A")
+			{
+				outcome = 1;
+			}
+			else if(right == "4")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 2;
+			}
+			break;
+		case "5":
+			if(right == "2" || right == "3" || right == "4")
+			{
+				outcome = 1;
+			}
+			else if(right == "5")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 2;
+			}
+			break;
+		case "6":
+			if(right == "2" || right == "3" || right == "4" || right == "5")
+			{
+				outcome = 1;
+			}
+			else if(right == "6")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 2;
+			}
+			break;
+		case "7":
+			if(right == "2" || right == "3" || right == "4" || right == "5" || right == "6")
+			{
+				outcome = 1;
+			}
+			else if(right == "7")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 2;
+			}
+			break;
+		case "8":
+			if(right == "2" || right == "3" || right == "4" || right == "5" || right == "6" || right == "7")
+			{
+				outcome = 1;
+			}
+			else if(right == "8")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 2;
+			}
+			break;
+		case "9":
+			if(right == "1" || right == "J" || right == "Q" || right == "K" || right == "A")
+			{
+				outcome = 2;
+			}
+			else if(right == "9")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 1;
+			}
+			break;
+		case "1":
+			if(right == "J" || right == "Q" || right == "K" || right == "A")
+			{
+				outcome = 2;
+			}
+			else if(right == "1")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 1;
+			}
+			break;
+		case "J":
+			if(right == "Q" || right == "K" || right == "A")
+			{
+				outcome = 2;
+			}
+			else if(right == "J")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 1;
+			}
+			break;
+		case "Q":
+			if(right == "K" || right == "A" || right == "2")
+			{
+				outcome = 2;
+			}
+			else if(right == "Q")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 1;
+			}
+			break;
+		case "K":
+			if(right == "A" || right == "2" || right == "3")
+			{
+				outcome = 2;
+			}
+			else if(right == "K")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 1;
+			}
+			break;
+		case "A":
+			if(right == "2" || right == "3" || right == "4")
+			{
+				outcome = 2;
+			}
+			else if(right == "A")
+			{
+				outcome = 3;
+			}
+			else
+			{
+				outcome = 1;
+			}
+			break;
 		}
 		
 		return outcome;
